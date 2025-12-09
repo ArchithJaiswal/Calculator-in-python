44240f2f588a0f3aab37b71addf12ad1f83c4f38
Archith Jaiswal <archithjaiswal@gmail.com>
2025-12-10 03:02:37 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+operator = input("Enter an operator + - * /:")
+num1 = float(input("Enter your first number : "))
+num2 = float(input("Enter your second number : "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print (num1 - num2)
+elif operator =="*":
+    print (num1 * num2)
+elif operator == "/":
+    print(num1/ num2)
+
